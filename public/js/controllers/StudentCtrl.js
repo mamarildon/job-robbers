91e8d5102ec689602525db28a5d3fdac9dda5eb1
Don Mamaril <mamarildon@gmail.com>
2014-06-14 19:38:52 -0700

    event listener placed

--- public/js/controllers/StudentCtrl.js
+++ public/js/controllers/StudentCtrl.js
@@ -1,17 +1,20 @@
 app.controller('StudentController', function ($scope, $rootScope, Students) {
-
+  $scope.tasks = [];
 
   $rootScope.$on('change:tasks', function (event, tasks) {
-    $scope.Applied          = tasks['Applied:'];
-    $scope.Graveyard        = tasks['Graveyard:'];
-    $scope.Leads            = tasks['Leads:'];
-    $scope.Offers           = tasks['Offers:'];
-    $scope.OnSiteScheduled  = tasks['On-Site Scheduled:'];
-    $scope.PostOnSite       = tasks['Post On-Site:'];
-    $scope.PostScreenConvo  = tasks['Post Screen Conversation:'];
-    $scope.PostTech         = tasks['Post Tech Screen (Phone/take home):'];
-    $scope.PreOnSite        = tasks['Pre-Onsite Tech Scren (phone/take home) Scheduled:'];
-    $scope.ScreenConvo      = tasks['Screen Conversation Scheduled:'];
+    $scope.tasks = tasks;
+    console.log($scope.tasks);
+
+    // $scope.Applied          = tasks['Applied:'];
+    // $scope.Graveyard        = tasks['Graveyard:'];
+    // $scope.Leads            = tasks['Leads:'];
+    // $scope.Offers           = tasks['Offers:'];
+    // $scope.OnSiteScheduled  = tasks['On-Site Scheduled:'];
+    // $scope.PostOnSite       = tasks['Post On-Site:'];
+    // $scope.PostScreenConvo  = tasks['Post Screen Conversation:'];
+    // $scope.PostTech         = tasks['Post Tech Screen (Phone/take home):'];
+    // $scope.PreOnSite        = tasks['Pre-Onsite Tech Scren (phone/take home) Scheduled:'];
+    // $scope.ScreenConvo      = tasks['Screen Conversation Scheduled:'];
   });
 
   $scope.logout = function () {
