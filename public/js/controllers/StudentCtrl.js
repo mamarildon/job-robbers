27b36b2c9433856d153221ebda641289eb3f1aca
Don Mamaril <mamarildon@gmail.com>
2014-06-14 19:19:59 -0700

    set scope vars to proper headers

--- public/js/controllers/StudentCtrl.js
+++ public/js/controllers/StudentCtrl.js
@@ -1,4 +1,18 @@
-app.controller('StudentController', function ($scope, Students) {
+app.controller('StudentController', function ($scope, $rootScope, Students) {
+
+
+  $rootScope.$on('change:tasks', function (event, tasks) {
+    $scope.Applied          = tasks['Applied:'];
+    $scope.Graveyard        = tasks['Graveyard:'];
+    $scope.Leads            = tasks['Leads:'];
+    $scope.Offers           = tasks['Offers:'];
+    $scope.OnSiteScheduled  = tasks['On-Site Scheduled:'];
+    $scope.PostOnSite       = tasks['Post On-Site:'];
+    $scope.PostScreenConvo  = tasks['Post Screen Conversation:'];
+    $scope.PostTech         = tasks['Post Tech Screen (Phone/take home):'];
+    $scope.PreOnSite        = tasks['Pre-Onsite Tech Scren (phone/take home) Scheduled:'];
+    $scope.ScreenConvo      = tasks['Screen Conversation Scheduled:'];
+  });
 
   $scope.logout = function () {
     Students.logout();
--- public/js/services/StudentService.js
+++ public/js/services/StudentService.js
@@ -1,31 +1,26 @@
-app.factory('Students', function ($http, $location, $rootScope) {
+app.factory('Students', function ($http, $rootScope, $location) {
 
-  $rootScope.$emit('change:tasks', function (tasks) {
+  var updateTasks = function (tasks) {
+    $rootScope.$emit('change:tasks', tasks);
+  };
 
-  });
 
   var parseData = function (tasks) {
     var results       = [];
     var currentHeader = '';
-
     tasks.forEach(function(task) {
       if (task.name.indexOf(':') !== -1) {
         currentHeader = task.name;
-
         results[task.name] = {
           id       : task.id,
           subTasks : []
         };
-
       } else {
         if (!!task.name.length) { results[currentHeader]['subTasks'].push(task); }
       }
     });
-
-    console.log(results);
-
+    updateTasks(results);
   };
-
 
   var Students = {};
 
